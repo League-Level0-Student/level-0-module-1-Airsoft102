a6a6f3b9089bb19c2ec055d24f436af961c354c1
dacoolkid <league@WTS13.attlocal.net>
2018-09-16 17:19:49 -0700

    Good day

---
+++ src/Unbirthday.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+public static void main(String[] args) {
+String input=JOptionPane.showInputDialog("what is your birthday");
+if (input.equals("09/16/18")) {
+JOptionPane.showMessageDialog(null, " happy birthday");
+}
+else {JOptionPane.showMessageDialog(null, "Have a merry UNbirthday");
+	
+
+	
+}
+}
+}
